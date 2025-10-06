9354c30ec1b6247d25071c04742f308fcbbab680
JoseISC <j.salazarcabello@uandresbello.edu>
2025-10-06 10:20:18 -0300

    delete failed project

--- mini-marketplace-unab/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Catalogo } from './catalogo/catalogo';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'catalogo', pathMatch: 'full' },
-  { path: 'catalogo', component: Catalogo }
-];
